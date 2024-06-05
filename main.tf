77634402c4318d2d8f71cd2c18377e5668fb9ab9
Rainer Halanek <rahalan@microsoft.com>
2024-06-05 09:10:10 +0200

    remove subnets

--- main.tf
+++ main.tf
@@ -25,18 +25,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-rahalan" # change to your rg name
   location = var.location
 }
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = var.vnetAdressSpace
-}
-
-resource "azurerm_subnet" "subnet" {
-  for_each             = var.subnets
-  name                 = each.value.name
-  address_prefixes     = [each.value.address]
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-}
