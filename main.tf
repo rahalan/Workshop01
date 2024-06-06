da85c092fd4a1fbda34d40904551c144ecb8ee35
Rainer Halanek <rahalan@microsoft.com>
2024-06-06 09:50:45 +0200

    deploy vnet

--- main.tf
+++ main.tf
@@ -19,3 +19,11 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+
+resource "azurerm_virtual_network" "name" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location = var.location
+  name = "vnet-${var.prefix}"
+  address_space = var.vnetAdressSpace
+}
