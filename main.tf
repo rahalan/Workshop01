715806fae043a400c7b6c0248cb9a62f29c2dac7
Rainer Halanek <rahalan@microsoft.com>
2024-06-07 08:24:48 +0200

    remove vnet

--- main.tf
+++ main.tf
@@ -20,17 +20,17 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  name                = "vnet-${var.prefix}"
-  address_space       = var.vnetAdressSpace
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = var.location
+#   name                = "vnet-${var.prefix}"
+#   address_space       = var.vnetAdressSpace
+# }
 
-resource "azurerm_subnet" "subnets" {
-  for_each             = var.subnets
-  name                 = each.value.name
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = each.value.address
-}
+# resource "azurerm_subnet" "subnets" {
+#   for_each             = var.subnets
+#   name                 = each.value.name
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = each.value.address
+# }
