3f6f5de0999917a18212e1db0c1a3a02d107198e
Rainer Halanek <rahalan@microsoft.com>
2024-06-07 08:23:24 +0200

    fix vnet reference

--- main.tf
+++ main.tf
@@ -31,6 +31,6 @@ resource "azurerm_subnet" "subnets" {
   for_each             = var.subnets
   name                 = each.value.name
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = each.value.address
 }
