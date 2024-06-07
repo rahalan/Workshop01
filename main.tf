0c609564395a6368c0ce5b09c89a470226deee3b
Rainer Halanek <rahalan@microsoft.com>
2024-06-07 09:35:22 +0200

    enable rbac on kv

--- main.tf
+++ main.tf
@@ -66,11 +66,12 @@ resource "azurerm_monitor_diagnostic_setting" "vnet-diagnostics" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                = "kv-${var.prefix}349787"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "standard"
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  name                      = "kv-${var.prefix}349787"
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.rg.name
+  sku_name                  = "standard"
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  enable_rbac_authorization = true
 }
 
 resource "azurerm_monitor_diagnostic_setting" "kv-diagnostics" {
